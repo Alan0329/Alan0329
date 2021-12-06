da51a6ac84e952d753b6fa0e25c4cd1c24adad6e
Alan0329 <87511672+Alan0329@users.noreply.github.com>
2021-12-06 21:14:53 +0800

    Update test.py

--- Other/DQN_Testing_Web_APP/test.py
+++ Other/DQN_Testing_Web_APP/test.py
@@ -202,5 +202,5 @@ else:
         st.subheader("Your Data")
         data = pd.read_csv("https://raw.githubusercontent.com/Alan0329/Alan0329/main/Other/DQN_Testing_Web_APP/00646.TW.csv")
         st.write(data)
-        param_file = "/DQN_Testing_Web_APP/200_checkpoint.pth"
+        param_file = "./DQN_Testing_Web_APP/200_checkpoint.pth"
         main(data, param_file)
